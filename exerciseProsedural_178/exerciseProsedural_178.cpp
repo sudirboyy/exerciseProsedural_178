a253990b35ef32f5ee32f422c629f6456c918ed8
sudirboyy <sudirboyy@gmail.com>
2023-03-27 15:16:43 +0700

    Exercise

--- exerciseProsedural_178/exerciseProsedural_178.cpp
+++ exerciseProsedural_178/exerciseProsedural_178.cpp
@@ -1,20 +1,40 @@
-// exerciseProsedural_178.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+double rerata(double a, double b) {
+    return (a + b) / 2;
+}
+    string status(double c) {
+        if (c >= 70)
+            return "Diterima";
+        else
+            return "Ditolak";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+ string status2(double rata, double nil) {
+        if (rata >= 70 || nil >= 80)
+            return "Diterima";
+        else
+            return "Ditolak";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+ int main()
+ {
+        char nama[30];
+        cout << "Masukan nama anda : ";
+        cin >> nama;
+        cout << endl;
+
+        double nilM, nilB;
+        cout << "Masukan nilai Matematika = ";
+        cin >> nilM;
+        cout << "Masukan nilai Bahasa Inggris = ";
+        cin >> nilB;
+
+        cout << "Nama Kandidat : " << nama << endl;
+
+        cout << "Status kelulusan = " << status(rerata(nilM, nilB));
+        cout << "\nStatus kelulusan ke 2 = " << status2(rerata(nilM, nilB), nilM);
+        return 0;
+    }
